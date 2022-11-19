d05f4e77f4e8a786e2b7dbe953317994978fd832
Clemens Horn <clemens.horn@mni.thm.de>
2022-11-19 19:50:45 +0100

    test ci
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ cargo clippy --release -- -D warnings &&
 CARGO_MANIFEST_DIR=$(pwd) cargo bootimage --release && 
 sleep 0.3 || exit 1
 cargo run --release &
-sleep 0.5
+sleep 0.3
 gvncviewer ::1:5900
