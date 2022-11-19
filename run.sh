cb26874b816695c78f7ba1e6f0beb9425759f612
Clemens Horn <clemens.horn@mni.thm.de>
2022-11-19 19:46:34 +0100

    run script works now better and should indicate if pipeline will fail or not
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-cargo run &
-sleep 0.3
+cargo fmt --all -- --check && 
+cargo clippy --release -- -D warnings &&
+CARGO_MANIFEST_DIR=$(pwd) cargo bootimage --release && 
+sleep 0.3 || exit 1
+cargo run --release &
+sleep 0.5
 gvncviewer ::1:5900
