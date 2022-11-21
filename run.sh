10e613f31aea36a76eb47bb206ff9bc95ddbffab
Clemens Horn <clemens.horn@mni.thm.de>
2022-11-21 11:30:50 +0100

    removed run.sh, because i figured out why qemu didnt work locally
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cargo fmt --all -- --check && 
-cargo clippy --release -- -D warnings &&
-CARGO_MANIFEST_DIR=$(pwd) cargo bootimage --release && 
-sleep 0.3 || exit 1
-cargo run --release &
-sleep 0.3
-gvncviewer ::1:5900
